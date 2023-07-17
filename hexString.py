2a40a9affa62eb9fe676c265c5431be1436546e3
Antony <antua.froloff@gmail.com>
2023-07-17 11:46:47 +0300

    some

--- hexString.py
+++ hexString.py
@@ -3,32 +3,31 @@ from Crypto.Cipher import AES
 import getpass
 import PySimpleGUI as sg
 from tkinter import *
+from tkinter import filedialog
 import json
 sg.theme('SystemDefault1')
 
 # charlyConfigPath = f"C:\\Users\\{getpass.getuser()}\\AppData\\Roaming\\Charles\\charles.config"
+keyMain = '5a677564567332496852716157716a376f774d774e5763314d766b6a36477548'
+
 
 def readDictPath(path):
     with open(path, "r", encoding='utf-8') as f:
         content = f.read()
     return content
-def decodeConfig(hex_bytes, key,  iv):
+def decodeConfig(hex_bytes, key,  ivStr):
     # hex_bytes = readDictPath(path)
     # for elem in [hex_bytes[i:i+31] for i in range(0, len(hex_bytes), 31)]:
     #     print(elem)
-
+    
     # конвертация hexadecimal-байтов в байты
     bytes = binascii.unhexlify(hex_bytes)
 
     # конвертация ключа в байты
     key = binascii.unhexlify(key)
 
-    # получение 32-байтового IV от пользователя
-    # iv = input("Введите 32-байтовый IV: ")
-    # iv = b'f115c755ed1e3dc1627306b3a95b2aba'
-
     # конвертация IV в байты
-    iv = binascii.unhexlify(iv)
+    iv = binascii.unhexlify(ivStr)
 
     # создание объекта AES cipher с mode-CBC
     cipher = AES.new(key, AES.MODE_CBC, iv)
@@ -36,15 +35,13 @@ def decodeConfig(hex_bytes, key,  iv):
     # декодирование байтов с помощью AES cipher
     decoded_bytes = cipher.decrypt(bytes)
     
-
     # конвертация декодированных байтов в строку
     decoded_string = decoded_bytes.decode('utf-8')
     # decoded_string = decoded_bytes.decode('utf-8')
     text_without_hex = ''.join(['' if ord(c) < 32 or ord(c) > 126 else c for c in decoded_string])
     parsed = json.loads(text_without_hex)
     prettyPrint = json.dumps(parsed, indent=4)
-    resultWindow(prettyPrint)
-
+    resultWindow(prettyPrint, ivStr)
 def encodeConfig(plaintext, key, iv):
     key_bytes = bytes.fromhex(key)
     iv_bytes = bytes.fromhex(iv)
@@ -53,12 +50,14 @@ def encodeConfig(plaintext, key, iv):
     cipher = AES.new(key_bytes, AES.MODE_CBC, iv_bytes)
     ciphertext = cipher.encrypt(padded_plaintext)
     encoded_ciphertext = ciphertext.hex()
-    startMenu(encoded_ciphertext)
+    
+    prettyPrint = '{' + '\n\t"data": {\n\t\t"dataHex": ' + f'"{encoded_ciphertext}",\n\t\t"ivHex": ' + f'"{iv}"' + '\n\t}' + '}'
+    startMenu(prettyPrint)
 
 
 def startMenu(textTo): 
     layout =  [
-        [sg.Input(size=(55,1), default_text="5a677564567332496852716157716a376f774d774e5763314d766b6a36477548", key='key', enable_events=True,
+        [sg.Input(size=(55,1), default_text=keyMain, key='key', enable_events=True,
                   readonly=True, disabled=True), sg.Text("64-byte key", text_color='green')],
         [sg.Multiline(f"""{textTo}""", size=(70,13), key='mLine', focus=True)],
         [sg.Button('decode', size=(12, 1)), sg.CloseButton('Close', size=(12, 1))]
@@ -68,13 +67,11 @@ def startMenu(textTo):
     while True:
         event, value = window.read(close=True)
         
-        # if event == 'go':
-        #     getConfig(value['hex'], value['key'], value['iv'])
-        
         if event == 'decode':
+            
             data = json.loads(value['mLine'])
-            decodeConfig(data['data']['dataHex'], value['key'], data['data']['ivHex'])
-        
+            ivMain = data['data']['ivHex']
+            decodeConfig(data['data']['dataHex'], keyMain, ivMain)
         
         if event in ('Close', None) or event == sg.WIN_CLOSED:
             try:
@@ -82,15 +79,41 @@ def startMenu(textTo):
             except:
                 break
         window.close()
-def resultWindow(resultText):
-
+        
+def resultWindow(resultText, iv):
+    def get_text():
+        getText = text.get("1.0", "end-1c")
+        root.destroy()
+        encodeConfig(getText, keyMain, iv)
+        return getText
+        
+    def save_text():
+        getText = text.get("1.0", "end-1c")
+        try:
+            path = filedialog.asksaveasfile(filetypes = (("Text files", "*.txt"), ("All files", "*.*"))).name
+            root.title('remoteConfig' + path)
+        except:
+            return   
+        with open(path, 'w') as f:
+            f.write(getText)
+    
+    def prompt():
+        sg.popup_notify("Настроить (Настройка популярного) --> hasMainscreenSettings = True")
+        
     #to create a window
     root = Tk()
     root.title('RemoteConfig')
     # root.geometry("600x900")
     
     #root window is the parent window
-    fram = Frame(root)
+    
+    fram = Frame(root)   
+    buttPrompt = Button(fram, text='prompt', command=prompt)
+    buttPrompt.pack(side=LEFT)
+    
+    buttSave = Button(fram, text='Save as', command=save_text)
+    buttSave.pack(side=LEFT)
+
 
     #adding label to search box
     Label(fram,text='Text to find: ',).pack(side=LEFT)
@@ -106,8 +129,9 @@ def resultWindow(resultText):
     edit.focus_set()
 # =================================================================
     # TEST
-    buttEncode = Button(fram, text='Encode')
+    buttEncode = Button(fram, text='Encode', command=get_text)
     buttEncode.pack(side=RIGHT)
+    
 # =================================================================
 
     buttClear = Button(fram, text='Clear') 
@@ -117,9 +141,7 @@ def resultWindow(resultText):
     buttFind.pack(side=RIGHT)
 
     fram.pack(side=TOP)
-    # fram.pack(side=TOP)
 
-    
     #  scrollBar
     v = Scrollbar(root, orient = 'vertical')
     v.pack(side=RIGHT, fill = 'y')
@@ -131,9 +153,6 @@ def resultWindow(resultText):
     # text.insert('1.0',open("C:\\Users\\frolov.an\\Desktop\\testHex.txt", 'r').read())
     text.insert("1.0", resultText)
     # text.config(state='disabled')
-
-    def encode():
-        pass
         
         
     #function to search string in text
@@ -170,11 +189,13 @@ def resultWindow(resultText):
 
         edit.focus_set()
    
-    buttEncode.config(command=encode)
+    buttEncode.config(command=get_text)
     buttClear.config(command=clear)
     buttFind.config(command=find)
+    # buttFind.config(command=save_text)
     
     root.mainloop()
+    root.destroy()
 
 
 
